7123ba9b4a0d6ccefb82239f1df00b5e12192d5a
jaeho3 <hojoh@sheridancollege.ca>
2021-01-29 11:57:03 -0500

    added String Address + getters and setters

--- src/student/Student.java
+++ src/student/Student.java
@@ -10,9 +10,17 @@ package student;
  * @author johnt
  */
 public class Student {
-
     private String name;
+    private String address;
 
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
     public String getName() {
         return name;
     }
