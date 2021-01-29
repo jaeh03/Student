cfc1c07f55827564587c186493011394cf304296
jaeho3 <hojoh@sheridancollege.ca>
2021-01-29 11:02:36 -0500

    First Commit

---
+++ src/student/Student.java
@@ -0,0 +1,28 @@
+/*
+ * John Ho
+ * Student ID: 991619682
+ * SYST10199 - Web Programming
+ */
+package student;
+
+/**
+ *
+ * @author johnt
+ */
+public class Student {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+
+
+        
+          
+}
---
+++ src/student/StudentList.java
@@ -0,0 +1,29 @@
+/*
+ * John Ho
+ * Student ID: 991619682
+
+ */
+package student;
+
+/**
+ *
+ * @author johnt
+ */
+public class StudentList {
+    public static void main(String[] args) {
+        Student []studentList = new [2] ;
+        
+        Student s1 = new Student(); 
+        s1.setName("Ronak");
+        
+        Student s2 = new Student(); 
+        s2.setName("Sheth");
+        
+        studentList[0] = s1;
+        studentList[1] = s2; 
+        
+        for(Student s : studentList){
+            System.out.println(s.getName());
+        }
+    }
+}
